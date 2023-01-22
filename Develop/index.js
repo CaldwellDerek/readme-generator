6daf471567bf851d61fb2b2a7b1929cf6113f07b
Derek Caldwell <djohncaldwell@gmail.com>
2023-01-22 13:23:24 -0800

    initial files created

---
+++ Develop/index.js
@@ -0,0 +1,23 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+        }
+    ).then(answers => {
+        console.log(answers);
+    })
+}
+
+// Function call to initialize app
+init();
