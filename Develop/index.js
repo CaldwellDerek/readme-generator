af1d3877ff438b007db59fef8bffdc0ed94fbddc
Derek Caldwell <djohncaldwell@gmail.com>
2023-01-22 14:59:01 -0800

    initial inquirer prompt created

--- Develop/index.js
+++ Develop/index.js
@@ -8,13 +8,60 @@ function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(
+    inquirer.prompt([
         {
             type: "input",
-            name: "name",
-            message: "What is your name?"
+            name: "Title",
+            message: "Enter a title for your repository:"
+        },
+        {
+            type: "input",
+            name: "Description",
+            message: "Enter a description of your project:"
+        },
+        {
+            type: "input",
+            name: "Installation",
+            message: "Enter the instructions to install your application:"
+        },
+        {
+            type: "input",
+            name: "Usage",
+            message: "Enter uses for your application:"
+        },
+        {
+            type: "list",
+            name: "License",
+            message: "What is your name?",
+            choices: ["Apache License", "GNU General Public License", "MIT License"]
+        },
+        {
+            type: "input",
+            name: "Contributing",
+            message: "How can someone contribute to your application?"
+        },
+        {
+            type: "input",
+            name: "Tests",
+            message: "What tests have been performed on your application?"
+        },
+        {
+            type: "input",
+            name: "Username",
+            message: "What is your GitHub username?"
+        },
+        {
+            type: "input",
+            name: "Email",
+            message: "What is your email address?"
+        },
+        {
+            type: "input",
+            name: "Questions",
+            message: "What is the best way to reach you with any questions?"
         }
-    ).then(answers => {
+    ])
+    .then(answers => {
         console.log(answers);
     })
 }
